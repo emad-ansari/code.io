941733b5c00e0ae7fc43e2a1b00e0ea3972475e0
92Mohammad <m.e89571828@gmail.com>
2024-07-20 17:20:31 +0530

    fixes some design issu

--- src/client/components/ProblemNavBar.tsx
+++ src/client/components/ProblemNavBar.tsx
@@ -10,15 +10,15 @@ export const ProblemNavBar = () => {
                 <img  src= {brandSvg} alt = 'Logo' className = 'w-56 h-16' />
             </div>
             <div className = 'flex flex-1 flex-row items-center justify-center gap-10 text-white'>
-                <div className = 'text-lg m-6 group relative w-max'>
+                <div className = 'text-sm m-6 group relative w-max'>
                     <Link to={'/problemset'}>Problems</Link>   
                     <NavLinkStyle />
                 </div>
-                <div className = 'text-lg m-6 group relative w-max'>
+                <div className = 'text-sm m-6 group relative w-max'>
                     <Link to={'/contests'}>Contests</Link>   
                     <NavLinkStyle />
                 </div>
-                <div className = 'text-lg m-6 group relative w-max'>
+                <div className = 'text-sm m-6 group relative w-max'>
                     <Link to={'/standings'}>Standings</Link>   
                     <NavLinkStyle />
                 </div>
