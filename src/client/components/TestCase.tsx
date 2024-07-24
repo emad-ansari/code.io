8d0553977c598d850e78cfe34e124b7c72044710
92Mohammad <m.e89571828@gmail.com>
2024-07-24 10:00:18 +0530

    testcase component created

--- src/client/components/TestCase.tsx
+++ src/client/components/TestCase.tsx
@@ -1,9 +1,17 @@
+interface TestCaseProps {
+    testCaseNumber: number;
+    input: string,
+    output: string
+}
 
-export const TestCase = () => {
+export const TestCase = ({testCaseNumber, input, output}: TestCaseProps) => {
     return (
-        <div className = 'flex flex-row  gap-3'>
-            <h1 className = 'text-3xl font-bold text-white'>Test Case {1}</h1>
-
+        <div className = 'flex flex-col  gap-3 w-full'>
+            <h1 className = 'text-xl font-semibold text-white'>Test Case {testCaseNumber + 1}</h1>
+            <span className="font-medium">Input</span>
+            <span className = 'rounded-md w-[80%] bg-white text-black h-14 flex justify-start items-center px-5 font-semibold'>{input}</span>
+            <span className = 'font-medium'>Output</span>
+            <span className = 'rounded-md w-[80%] bg-white text-black h-14 flex justify-start items-center px-5 font-semibold'>{output}</span>
         </div>
     )
 }
--- src/client/pages/Editor.tsx
+++ src/client/pages/Editor.tsx
@@ -1,15 +1,25 @@
 import { TestCase} from '../components/TestCase'
+import { testcase } from '../../utils/testcase'
 
 export const Editor = () => {
     return (
         <div className = 'flex gap-3 fixed left-0 right-0 bottom-0 top-[68px] px-3 py-3 '>
-            <section  className="flex flex-1 gap-8 flex-col rounded-lg text-white bg-[#484848] justify-start px-6">
-                <div className = 'flex flex-col gap-[36px] pt-5'>
-                    <h1 className = 'text-3xl'> Two sum</h1>
+            <section  className="flex flex-1 gap-8 flex-col rounded-lg text-white bg-[#484848] justify-start px-6 py-3 overflow-scroll scroll-smooth">
+                <div className = 'flex flex-col gap-[28px] pt-5'>
+                    <h1 className = 'text-3xl font-bold'> Two sum</h1>
                     <span>finding the sum of two elements</span>
                 </div>
-                <div className = 'flex '>
-                    <TestCase/>
+                <div className = 'flex flex-col gap-8 '>
+                    {
+                        testcase.map((testcase, index) => {
+                            return <TestCase 
+                                key = {testcase.id}
+                                testCaseNumber = {index}
+                                input = {testcase.input}
+                                ouput = {testcase.output}
+                            />
+                        })
+                    }
                     
                 </div>
                 
