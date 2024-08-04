1cfb3aafe699cae94020c3285d32904e5b0379de
92Mohammad <m.e89571828@gmail.com>
2024-08-04 22:45:25 +0530

    login theme changes

--- src/client/pages/SignupPage.tsx
+++ src/client/pages/SignupPage.tsx
@@ -1,24 +1,24 @@
 import { Button } from "../components/Button"
-import logo from '../../assets/logo.png'
+import logo from '../../assets/siginLogo.svg'
 import { EmailInputField } from "../components/EmailInputField"
 import { PasswordInputField } from "../components/PasswordInputField"
 import { FcGoogle } from "react-icons/fc";
 
 export const SignupPage = () => {
     return (
-        <main className = 'bg-[#141515]  fixed top-0 right-0 left-0 bottom-0 flex justify-center pt-32'>
+        <main className = 'bg-[#030303]  fixed top-0 right-0 left-0 bottom-0 flex justify-center pt-32'>
             
-        <div className="w-[350px] h-[400px] md:w-[450px] md:h-[500px] bg-[#00242C] rounded-lg flex flex-col items-center pt-5">
+        <div className="w-[350px] h-[400px] md:w-[450px] md:h-[500px]  bg-[#0D1621] rounded-lg flex flex-col items-center ">
             <img 
                 src= {logo}
                 alt="logo"
-                className='w-[150px] h-[80px] object-cover'
+                className='w-[110px] h-[110px]'
             />
             <h1 className='text-white text-xl font-medium'>Create your account</h1>
             <div className = 'flex flex-col gap-5 pt-5  w-[350px]'>
                 <EmailInputField /> 
                 <PasswordInputField/>
-                <Button  classname='w-full bg-[#81E291] text-sm font-medium' >Sign up</Button> 
+                <Button  classname='w-full bg-gray-400 text-sm font-medium' >Sign up</Button> 
                 <div className='flex flex-row gap-4 items-center justify-between'>
                     <hr className='w-40 h-[1px]  bg-gray-200 border-0 dark:bg-gray-500' />
                     <span className = 'text-white text-sm'>OR</span>
