003f30f981e2f53de617c7bbe1374af9ce2240e1
92Mohammad <m.e89571828@gmail.com>
2024-08-16 10:36:20 +0530

    fixes url and filter problem

--- src/client/components/DifficultyDroptDownMenu.tsx
+++ src/client/components/DifficultyDroptDownMenu.tsx
@@ -13,24 +13,27 @@ export const DifficultyDropDownMenu = () => {
 	);
 	const { easy, medium, hard} = useSelector((state: RootState) => state.filter);
 	const [searchParams, setSearchParams] = useSearchParams();
+
 	const currentPageNumber = searchParams.get("page");
-	console.log(easy, medium , hard);
+	
 
-	const filterProblems = ( difficultyLevel: string, isFilterApply: boolean ) => {
-		// const option: string = difficultyLevel.toLocaleLowerCase();
+	const filterProblems = ( difficultyLevel: string) => {		
+		if (searchParams.get('difficulty') === difficultyLevel){
+			// remove it from url
+			searchParams.delete('difficulty');
+		}
+		else {
+			// add it
+			searchParams.set('difficulty', difficultyLevel);
+		}
+		setSearchParams(searchParams);
 		
-		console.log('checking whehter the filter is apply or not: ', isFilterApply);
-		setSearchParams({
-			page: currentPageNumber !== null ? currentPageNumber : "1",
-			difficulty: isFilterApply ? difficultyLevel: "",
-		});
-
 		dispatch(
 			getProblems({
 				pageNumber: currentPageNumber !== null ? Number(currentPageNumber) : 1,
-				difficultyLevel: isFilterApply ? difficultyLevel : ""
+				difficultyLevel: searchParams.get('difficulty') !== null ? String(searchParams.get('difficulty')) : ""
 			})
-		);
+		);		
 	};
 
 	return (
@@ -49,18 +52,19 @@ export const DifficultyDropDownMenu = () => {
 interface DropDownItemProps {
 	value: string;
 	isFilterApply: boolean;
-	filterProblems: (difficultyLevel: string, isfilterApply: boolean) => void;
+	filterProblems: (difficultyLevel: string) => void;
 	icons?: IconType; // for future use
 }
 
-function DropDownItem({value, filterProblems, isFilterApply}: DropDownItemProps) {
+export function DropDownItem({value, filterProblems, isFilterApply}: DropDownItemProps) {
 	const dispatch = useAppDispatch();
+	
 	return (
 		<span
-			className="text-[#0FA958] font-normal hover:bg-hover flex items-center px-2 py-2 w-[90%] rounded-md justify-between"
+			className="font-normal hover:bg-hover flex items-center px-2 py-2 w-[90%] rounded-md justify-between"
 			onClick={() => {
-				dispatch(setFilterOptions({option: value.toLocaleLowerCase()}));
-				filterProblems(value, isFilterApply);
+				dispatch(setFilterOptions({option: value.toLocaleLowerCase()}));	
+				filterProblems(value)
 			}}
 		>
 			<span className= {`${value === "Easy" ? 'text-GREEN' : value === "Medium" ? 'text-YELLOW': "text-RED"}`}>{value}</span>
