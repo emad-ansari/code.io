12e642945aeaf84e4d171b6314238f3a96987cb2
92Mohammad <m.e89571828@gmail.com>
2024-09-17 14:52:53 +0530

    return normal testcases array without modification

--- src/backend/src/lib/testcaseParser.ts
+++ src/backend/src/lib/testcaseParser.ts
@@ -1,21 +1,24 @@
 import * as fs from "fs";
 import { TestCase } from "../db/testcase";
 
+
+
 export class TestCaseParser {
-	testcaseId: string = "";
+	userId: string = "";
 	title: string = "";
 	testcases: TestCase[] = [];
 
 	extractTestCaseDetails(filePath: string) {
 		const fileContent = fs.readFileSync(filePath, "utf-8");
 		const data = JSON.parse(fileContent);
-		this.testcaseId = data.testcaseId;
+		this.userId = data.userId;
 		this.title = data.title;
 		this.testcases = data.testcases;
 		return {
-			id: this.testcaseId,
+			userId: this.userId,
 			title: this.title,
 			testcases: this.testcases,
 		};
 	}
+
 }
