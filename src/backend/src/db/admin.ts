85a26a0a8f64678957a23c2a151c3120daa191fb
92Mohammad <m.e89571828@gmail.com>
2024-09-17 12:00:18 +0530

    created findAdmin to check whether admin exist or not

--- src/backend/src/db/admin.ts
+++ src/backend/src/db/admin.ts
@@ -1,15 +1,19 @@
 import prisma from "../db/index";
 
-
+import bcrypt from 'bcrypt'
 
 
 export async function createAdmin (username: string, email: string, password: string): Promise<{ success: boolean, msg: string}> {
+
    try {
+        // hash the password 
+        const hashedPassword = await bcrypt.hash(password, 10)
+
         const newAdmin = await prisma.admin.create({
             data: {
                 username,
                 email,
-                password
+                password: hashedPassword
             },
             select: {
                 id: true
@@ -34,6 +38,50 @@ export async function createAdmin (username: string, email: string, password: st
         return {
             success: false,
             msg: error.message    
+        }
+    }
+}
+
+export async function findAdmin (email: string, password: string): Promise<{ success: boolean, msg: string, adminId?: string}>{
+    try {   
+        const admin = await prisma.admin.findFirst({
+			where: {
+				email
+			},
+			select: {
+				password: true,
+                id: true
+			}
+		})
+		if (!admin){
+			return {
+                success: false,
+                msg: "Admin Not found"
+            }
+		}
+		else {
+			// match the password
+			// so first need to decrypt it
+			const matchPassword = await bcrypt.compare(password, admin.password)
+            if (matchPassword){
+                return {
+                    success: true,
+                    msg: "Admin found",
+                    adminId: admin.id
+                }
+            }
+            else {
+                return {
+                    success: false,
+                    msg: "Password does not match"
+                }
+            }
+		}
+    }
+    catch(error: any){
+        return {
+            success: false,
+            msg: error.message
         }
     }
 }
