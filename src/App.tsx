d5d4df1cb453f5ead1731665f336b77c43ef9cf2
92Mohammad <m.e89571828@gmail.com>
2024-09-18 11:34:11 +0530

    fixes route problem

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,19 @@ import { StandingPage } from "./client/pages/StandingPage";
 import { ProblemDescriptionPage } from "./client/pages/ProblemDescriptionPage";
 import { ProblemList } from "./client/components/common/ProblemList";
 import { AboutUs } from "./client/pages/AboutUsPage";
-import { Layout } from "./client/layouts/Layout";
 import { ProblemStatement }  from "./client/components/common/ProblemDescription";
 import { ProblemSubmissions }  from "./client/components/common/ProblemSubmissions";
 import { Contribution } from "./client/pages/Contribution";
 import { TestCaseForm } from "./client/pages/TestCaseForm";
 import { ProblemForm } from "./client/pages/ProblemForm";
-
+import { ProblemNavBar } from "./client/components/common/ProblemNavBar";
 import "./index.css";
 
 const App = () => {
 	return (
 		<>
 			<BrowserRouter>
+				<ProblemNavBar isProbleDescriptioPage={false} />
 				<Routes>
 					<Route index element={<HomePage />} />
 					<Route path="/login" element={<LoginPage />} />
@@ -30,23 +30,20 @@ const App = () => {
 						path="/verify-email"
 						element={<EmailAuthenticaiton />}
 					/>
-					<Route path="/about-us" element={<AboutUs />} />
-					<Route path="/p" element={<Layout />}>
-						<Route path="problemset" element={<ProblemsetPage />}>
-							<Route index element={<ProblemList />} />
-						</Route>
-						<Route path="contests" element={<ContestPage />} />
-						<Route path="standings" element={<StandingPage />} />
+					<Route path="/problemset" element={<ProblemsetPage />}>
+						<Route index element={<ProblemList />} />
+					</Route>
+					<Route path="/contests" element={<ContestPage />} />
+					<Route path="/standings" element={<StandingPage />} />
+					<Route path="/contribution" element={<Contribution />} >
+						<Route index  element = {<ProblemForm />} />
+						<Route path = 'testcase' element = {<TestCaseForm />} />
 					</Route>
 					<Route path="problem/:problem-name/" element={<ProblemDescriptionPage />}>
 						<Route  path = 'description' element={<ProblemStatement />} />
-						<Route  path = 'submissions' element={< ProblemSubmissions />} />
-						
+						<Route  path = 'submissions' element={< ProblemSubmissions />} />	
 					</Route>
-					<Route path = 'contribution' element = {<Contribution />}> 
-						<Route path = 'problem' element = {<ProblemForm/>}/> 
-						<Route path = 'testcase' element = {<TestCaseForm/>}/> 
-					</Route>
+					<Route path="/about-us" element={<AboutUs />} />
 				</Routes>
 			</BrowserRouter>
 		</>
