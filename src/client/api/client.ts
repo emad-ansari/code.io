9065a7a1012833fc3e01d7cee9dd67c1776b2c8a
92Mohammad <m.e89571828@gmail.com>
2024-09-19 15:40:58 +0530

    added authorization header

--- src/client/api/client.ts
+++ src/client/api/client.ts
@@ -4,6 +4,7 @@ export const client = axios.create({
     baseURL: 'http://localhost:3000/api',
     timeout: 10000,
     headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem('CToken')
     }
 });
