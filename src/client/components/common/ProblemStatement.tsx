145ef2d27af0eab4186c0bee1ba1203abe91d315
92Mohammad <m.e89571828@gmail.com>
2024-10-13 23:47:15 +0530

    fix problem status display bug for todo status

--- src/client/components/common/ProblemStatement.tsx
+++ src/client/components/common/ProblemStatement.tsx
@@ -3,23 +3,29 @@ import { fetchProblemDetail } from "@/client/features/problemSlice";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 import { TestCaseExample } from "./TestCaseExample";
-import { BiAdjust } from "react-icons/bi";
-import { ListTodo } from "lucide-react";;
+import { CircleCheckBig, Contrast } from "lucide-react";
+import { problems } from "@/client/pages/problems";
 
 export default function ProblemStatement() {
-	
 	const { title } = useParams();
-	
+
 	const { isLogin } = useSelector((state: RootState) => state.user);
-	const { problemDetail } = useSelector((state: RootState) => state.problem);
-	const { description, problemNo, difficulty, problemStatus , testcaseExamples} = problemDetail;
-	
+	const { problemDetail, loading } = useSelector(
+		(state: RootState) => state.problem
+	);
+	const {
+		description,
+		problemNo,
+		difficulty,
+		problemStatus,
+		testcaseExamples,
+	} = problemDetail;
+
 	const dispatch = useAppDispatch();
 	useEffect(() => {
 		if (title) {
-			dispatch(fetchProblemDetail({title: title}));
+			dispatch(fetchProblemDetail({ title: title }));
 		} else {
 			console.log("problem id is not undefined");
 		}
@@ -30,36 +36,46 @@ export default function ProblemStatement() {
 			<div className="flex flex-col gap-5 pt-5 ">
 				<div className="flex flex-row items-center justify-between">
 					<span className="flex gap-2 text-[26px] font-semibold">
-						{problemNo !== 0 && (
-							<span>{problemNo}.</span>
-						)}
+						{problemNo !== 0 && <span>{problemNo}.</span>}
 						<span>{problemDetail.title}</span>
 					</span>
 					<div className="flex flex-row gap-4">
-						<span
-							className={`font-normal text-sm  bg-gray-800 px-3 py-1 rounded-full ${
-								difficulty == "Easy"
-									? "text-[#4ac3ab]"
-									: difficulty == "Medium"
-									? "text-YELLOW"
-									: "text-RED"
-							} `}
-						>
-							{difficulty}
-						</span>
-						{isLogin && (
+						{!loading && (
+							<span
+								className={`font-normal text-sm  bg-gray-800 px-3 py-1 rounded-full ${
+									difficulty == "Easy"
+										? "text-[#4ac3ab]"
+										: difficulty == "Medium"
+										? "text-YELLOW"
+										: "text-RED"
+								} `}
+							>
+								{difficulty}
+							</span>
+						)}
+
+						{isLogin && !loading && problemStatus?.status !== "Todo" &&  (
 							<div className="flex flex-row gap-1 items-center  bg-gray-800 rounded-full px-3 py-1">
-								<span className="text-sm">{problemStatus?.status}</span>
+								<span className="text-sm">
+									{problemStatus?.status}
+								</span>
 								<span className="flex items-center">
 									{problemStatus?.status === "Solved" ? (
-										<IoMdCheckmarkCircleOutline className="text-[#4ac3ab]" />
-									) : problemStatus?.status === "Attempted" ? (
-										<BiAdjust className="text-YELLOW" />
-									) : (
-										<ListTodo size={16} />
-									)}
+										<CircleCheckBig
+											size={16}
+											absoluteStrokeWidth
+											className="text-[#4ac3ab]"
+										/>
+									) : problemStatus?.status ===
+									  "Attempted" ? (
+										<Contrast
+											size={16}
+											absoluteStrokeWidth
+											className="text-YELLOW"
+										/>
+									) : null}
 								</span>
-							</div>
+							</div>	
 						)}
 					</div>
 				</div>
