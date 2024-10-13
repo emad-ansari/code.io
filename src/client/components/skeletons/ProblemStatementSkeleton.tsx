64921619a60d0fd63ca924b54b67f4d396ef2ee6
92Mohammad <m.e89571828@gmail.com>
2024-10-13 23:51:45 +0530

    changes skeleton of problem description

--- src/client/components/skeletons/ProblemStatementSkeleton.tsx
+++ src/client/components/skeletons/ProblemStatementSkeleton.tsx
@@ -5,15 +5,20 @@ export const ProblemStatementSkeleton = () => {
 		<div className="flex flex-col gap-4 px-5 pb-3 overflow-y-scroll scroll-smooth h-[93%]">
 			<div className="flex flex-col gap-5 pt-5 ">
 				<div className="flex flex-row items-center justify-between">
-                    <Skeleton className="h-6 w-[200px] bg-gray-800 rounded" />
+					<Skeleton className="h-6 w-[200px] bg-gray-800 rounded" />
 					<div className="flex gap-4">
-                        <Skeleton className="h-6 w-[100px] bg-gray-800 rounded-full" />
+						<Skeleton className="h-6 w-[100px] bg-gray-800 rounded-full" />
 						<Skeleton className="h-6 w-[100px] bg-gray-800 rounded-full" />
 					</div>
 				</div>
-				<Skeleton className="h-32 w-[90%] bg-gray-800" />
+				<div className="flex flex-col gap-3">
+					<Skeleton className="h-3 w-[100%] bg-gray-800" />
+					<Skeleton className="h-3 w-[100%] bg-gray-800" />
+					<Skeleton className="h-3 w-[100%] bg-gray-800" />
+					<Skeleton className="h-3 w-[50%] bg-gray-800" />
+				</div>
 			</div>
-			<div className="flex flex-col gap-8 ">
+			<div className="flex flex-col gap-8 mt-4">
 				<div className="flex flex-col gap-4">
 					<Skeleton className="h-5 w-[120px] bg-gray-800 rounded" />
 					<Skeleton className="h-16 w-[90%] bg-gray-800 rounded" />
