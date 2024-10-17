517c9c052014f04fbe7a5a2ea7275f912ff1b6de
92Mohammad <m.e89571828@gmail.com>
2024-10-17 12:14:15 +0530

    wraped all Routes inside suspense api to prevent application break when going back using chrome back button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { lazy } from "react";
+import { lazy, useEffect, Suspense } from "react";
 import { LoginPage } from "./client/pages/LoginPage";
 import { SignupPage } from "./client/pages/SignupPage";
 import { EmailAuthenticaiton } from "./client/pages/EmailAuthenticaiton";
@@ -11,15 +11,21 @@ import { TestCaseForm } from "./client/pages/TestCaseForm";
 import { ProblemForm } from "./client/pages/ProblemForm";
 import { ProblemNavBar } from "./client/components/common/ProblemNavBar";
 import "./index.css";
-import { useEffect } from "react";
+
+const HomePage = lazy(() => import("./client/pages/HomePage"));
+const ProblemStatement = lazy(() =>
+	wait(2000).then(() => import("./client/components/common/ProblemStatement"))
+);
+const ProblemsetPage = lazy(() => import("./client/pages/ProblemsetPage"));
+const ProblemList = lazy(() =>
+	wait(1000).then(() => import("./client/components/common/ProblemList"))
+);
+const ProblemDescriptionPage = lazy(
+	() => import("./client/pages/ProblemDescriptionPage")
+);
+
 import { rehydrateAuth } from "./client/features/authSlice";
 import { useAppDispatch } from "./client/app/store";
-
-const HomePage = lazy(() => import("./client/pages/HomePage"))
-const ProblemStatement = lazy(() => wait(2000).then(() => import("./client/components/common/ProblemStatement")))
-const ProblemsetPage = lazy(() => import( "./client/pages/ProblemsetPage"))
-const ProblemList = lazy(() => wait(2000).then(() => import("./client/components/common/ProblemList")));
-const ProblemDescriptionPage = lazy(() => import ( "./client/pages/ProblemDescriptionPage"))
 
 const App = () => {
 	const dispatch = useAppDispatch();
@@ -32,44 +38,47 @@ const App = () => {
 		<>
 			<BrowserRouter>
 				<ProblemNavBar isProbleDescriptioPage={false} />
-				<Routes>
-					<Route index element={<HomePage />} />
-					<Route path="/login" element={<LoginPage />} />
-					<Route path="/signup" element={<SignupPage />} />
-					<Route
-						path="/verify-email"
-						element={<EmailAuthenticaiton />}
-					/>
-					<Route path="/problemset" element={<ProblemsetPage />}>
-						<Route index element={<ProblemList />} />
-					</Route>
-					<Route path="/contests" element={<ContestPage />} />
-					<Route path="/standings" element={<StandingPage />} />
-					<Route path="/contribution" element={<Contribution />}>
-						<Route index element={<ProblemForm />} />
-						<Route path="testcase" element={<TestCaseForm />} />
-					</Route>
-					<Route
-						path="problem/:title/"
-						element={<ProblemDescriptionPage />}
-					>
+				<Suspense>
+					<Routes>
+						<Route index element={<HomePage />} />
+						<Route path="/login" element={<LoginPage />} />
+						<Route path="/signup" element={<SignupPage />} />
 						<Route
-							path="description"
-							element={<ProblemStatement />}
+							path="/verify-email"
+							element={<EmailAuthenticaiton />}
 						/>
+						<Route path="/problemset" element={<ProblemsetPage />}>
+							<Route index element={<ProblemList />} />
+						</Route>
+						<Route path="/contests" element={<ContestPage />} />
+						<Route path="/standings" element={<StandingPage />} />
+						<Route path="/contribution" element={<Contribution />}>
+							<Route index element={<ProblemForm />} />
+							<Route path="testcase" element={<TestCaseForm />} />
+						</Route>
 
 						<Route
-							path="submissions"
-							element={<ProblemSubmissions />}
-						/>
-					</Route>
-				</Routes>
+							path="problem/:title/"
+							element={<ProblemDescriptionPage />}
+						>
+							<Route
+								path="description"
+								element={<ProblemStatement />}
+							/>
+
+							<Route
+								path="submissions"
+								element={<ProblemSubmissions />}
+							/>
+						</Route>
+					</Routes>
+				</Suspense>
 			</BrowserRouter>
 		</>
 	);
 };
 
-function wait (time: number){
-	return new Promise(resolve => setTimeout(resolve, time));
+function wait(time: number) {
+	return new Promise((resolve) => setTimeout(resolve, time));
 }
 export default App;
