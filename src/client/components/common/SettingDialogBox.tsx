45e52d9135a3b1bd9dd27ab2c2237db98621f6b7
92Mohammad <m.e89571828@gmail.com>
2024-10-19 22:57:46 +0530

    replace mui dialog to shadcnui dialog

---
+++ src/client/components/common/SettingDialogBox.tsx
@@ -0,0 +1,126 @@
+import { Button } from "../ui/button";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "../ui/dialog";
+
+import { Label } from "../ui/label";
+
+import { useSelector } from "react-redux";
+import { RootState, useAppDispatch } from "@/client/app/store";
+import {
+	setFontSize,
+	setTheme,
+} from "../../features/editorSettingSlice";
+import { Settings } from "lucide-react";
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "../ui/select";
+
+const EDITOR_THEMES = ["vs-dark", "vs", "hc-light", "OneDarkPro"];
+const FONT_SIZES = ["12px", "14px", "16px", "18px", "20px", "22px"];
+
+export default function SettingDialogBox() {
+	const dispatch = useAppDispatch();
+    const { fontSize } = useSelector((state: RootState) => state.setting)
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button
+					size="icon"
+					className="rounded-full hover:bg-gray-800"
+				>
+					<Settings size={16} className="text-white" />
+				</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px] bg-darkGray text-white border border-BORDER">
+				<DialogHeader>
+					<DialogTitle>Editor Playground Setting</DialogTitle>
+					<DialogDescription>
+						You can change the theme and font size.
+					</DialogDescription>
+				</DialogHeader>
+				<div className="flex flex-col gap-3 py-4">
+					<div className="flex flex-row  items-center justify-between">
+						<Label htmlFor="font-size" className="flex">
+							Font Size:
+						</Label>
+						<Select
+							onValueChange={(value) => {
+								const newFontSize = Number(
+									value.substring(0, 2)
+								);
+								dispatch(setFontSize(newFontSize));
+							}}
+						>
+							<SelectTrigger className="w-[80%] text-white border border-none bg-gray-800">
+								<SelectValue
+									placeholder={`${fontSize}px`}
+									className="text-white"
+								/>
+							</SelectTrigger>
+							<SelectContent className="bg-darkGray text-white border border-BORDER">
+								<SelectGroup className="">
+									{FONT_SIZES.map((item, index) => {
+										return (
+											<SelectItem
+												value={item}
+												key={index}
+												className="cursor-pointer"
+											>
+												{item}
+											</SelectItem>
+										);
+									})}
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+					</div>
+					<div className="flex flex-row gap-5 items-center justify-between">
+						<Label htmlFor="theme" className="flex flex-1">
+							Theme:
+						</Label>
+						<Select
+							onValueChange={(value) => dispatch(setTheme(value))}
+						>
+							<SelectTrigger className="w-[80%] text-white border border-none bg-gray-800">
+								<SelectValue
+									placeholder="vs-dark"
+									className="texxt-white"
+								/>
+							</SelectTrigger>
+							<SelectContent className="bg-darkGray text-white border border-BORDER">
+								<SelectGroup className="">
+									{EDITOR_THEMES.map((item, index) => {
+										return (
+											<SelectItem
+												value={item}
+												key={index}
+												className="cursor-pointer"
+											>
+												{item}
+											</SelectItem>
+										);
+									})}
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+					</div>
+				</div>
+				{/* <DialogFooter>
+					<Button type="submit">Save changes</Button>
+				</DialogFooter> */}
+			</DialogContent>
+		</Dialog>
+	);
+}
