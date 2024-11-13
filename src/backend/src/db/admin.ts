935c2a3b86f4afcbe4949ea816a65401a5049a33
92Mohammad <m.e89571828@gmail.com>
2024-11-13 22:57:40 +0530

    assign role to user after check whether user is admin or not while creating user

--- src/backend/src/db/admin.ts
+++ src/backend/src/db/admin.ts
@@ -1,87 +1,87 @@
-import prisma from "../db/index";
+// import prisma from "../db/index";
 
-import bcrypt from 'bcrypt'
+// import bcrypt from 'bcrypt'
 
 
-export async function createAdmin (username: string, email: string, password: string): Promise<{ success: boolean, msg: string}> {
+// export async function createAdmin (username: string, email: string, password: string): Promise<{ success: boolean, msg: string}> {
 
-   try {
-        // hash the password 
-        const hashedPassword = await bcrypt.hash(password, 10)
+//    try {
+//         // hash the password 
+//         const hashedPassword = await bcrypt.hash(password, 10)
 
-        const newAdmin = await prisma.admin.create({
-            data: {
-                username,
-                email,
-                password: hashedPassword
-            },
-            select: {
-                id: true
-            }
-        })
-        if (newAdmin){
-            console.log("admin reated successfully");
-            return {
-                success: true,
-                msg: "admin reated successfully"
-            }
-        }
-        else {
-            return {
-                success: false,
-                msg: "Not able to create admin"    
-            }
-        }
+//         const newAdmin = await prisma.admin.create({
+//             data: {
+//                 username,
+//                 email,
+//                 password: hashedPassword
+//             },
+//             select: {
+//                 id: true
+//             }
+//         })
+//         if (newAdmin){
+//             console.log("admin reated successfully");
+//             return {
+//                 success: true,
+//                 msg: "admin reated successfully"
+//             }
+//         }
+//         else {
+//             return {
+//                 success: false,
+//                 msg: "Not able to create admin"    
+//             }
+//         }
 
-    }
-    catch(error: any){
-        return {
-            success: false,
-            msg: error.message    
-        }
-    }
-}
+//     }
+//     catch(error: any){
+//         return {
+//             success: false,
+//             msg: error.message    
+//         }
+//     }
+// }
 
-export async function findAdmin (email: string, password: string): Promise<{ success: boolean, msg: string, adminId?: string}>{
-    try {   
-        const admin = await prisma.admin.findFirst({
-			where: {
-				email
-			},
-			select: {
-				password: true,
-                id: true
-			}
-		})
-		if (!admin){
-			return {
-                success: false,
-                msg: "Admin Not found"
-            }
-		}
-		else {
-			// match the password
-			// so first need to decrypt it
-			const matchPassword = await bcrypt.compare(password, admin.password)
-            if (matchPassword){
-                return {
-                    success: true,
-                    msg: "Admin found",
-                    adminId: admin.id
-                }
-            }
-            else {
-                return {
-                    success: false,
-                    msg: "Password does not match"
-                }
-            }
-		}
-    }
-    catch(error: any){
-        return {
-            success: false,
-            msg: error.message
-        }
-    }
-}
+// export async function findAdmin (email: string, password: string): Promise<{ success: boolean, msg: string, adminId?: string}>{
+//     try {   
+//         const admin = await prisma.admin.findFirst({
+// 			where: {
+// 				email
+// 			},
+// 			select: {
+// 				password: true,
+//                 id: true
+// 			}
+// 		})
+// 		if (!admin){
+// 			return {
+//                 success: false,
+//                 msg: "Admin Not found"
+//             }
+// 		}
+// 		else {
+// 			// match the password
+// 			// so first need to decrypt it
+// 			const matchPassword = await bcrypt.compare(password, admin.password)
+//             if (matchPassword){
+//                 return {
+//                     success: true,
+//                     msg: "Admin found",
+//                     adminId: admin.id
+//                 }
+//             }
+//             else {
+//                 return {
+//                     success: false,
+//                     msg: "Password does not match"
+//                 }
+//             }
+// 		}
+//     }
+//     catch(error: any){
+//         return {
+//             success: false,
+//             msg: error.message
+//         }
+//     }
+// }
