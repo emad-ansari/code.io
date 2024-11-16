a5614e07ecac3ab421ad923e461c3c06ca54d608
92Mohammad <m.e89571828@gmail.com>
2024-11-16 11:02:34 +0530

    chnage the problem form layout and design

--- src/client/pages/ProblemForm.tsx
+++ src/client/pages/ProblemForm.tsx
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 import { useAppDispatch, RootState } from "../app/store";
 import { Button } from "../components/common/Button";
 import { IoAdd } from "react-icons/io5";
-import { MdDelete } from "react-icons/md";
+
 import { TestCaseContainer } from "../components/common/TestCaseContainer";
 import { ContributionExample } from "../components/common/ContributionExample";
 import {
@@ -13,6 +13,7 @@ import {
 	TooltipProvider,
 	TooltipTrigger,
 } from "../components/ui/tooltip";
+import { Input } from "../components/ui/input";
 
 import {
 	setTitle,
@@ -35,16 +36,15 @@ import {
 	SelectValue,
 } from "../components/ui/select";
 import { typeOptions } from "../lib/types";
+import { TestCaseForm } from "./TestCaseForm";
 
-export const ProblemForm = () => {
+const ProblemForm = () => {
 	const dispatch = useAppDispatch();
-	const { title, description, parameters } = useSelector(
+	const { title, description } = useSelector(
 		(state: RootState) => state.problemform
 	);
-	const { testcases } = useSelector((state: RootState) => state.TestCaseForm);
+	
 	const { isLogin } = useSelector((state: RootState) => state.user);
-
-	const difficultyOption: string[] = ["Easy", "Medium", "Hard"];
 
 	const handleParameters = () => {
 		const newParameter = {
@@ -55,32 +55,17 @@ export const ProblemForm = () => {
 		dispatch(addParameter(newParameter));
 	};
 
-	const handleNewTestCase = () => {
-		const newTestCase = {
-			testcaseId: uuidv4(),
-			inputs: [
-				{
-					inputId: uuidv4(),
-					name: "",
-					type: "",
-					value: "",
-				},
-			],
-			output: {
-				type: "",
-				value: "",
-			},
-		};
-		dispatch(addNewTestCase(newTestCase));
-	};
+
 
 	return (
-		<div className="bg-PRIMARY  flex flex-row justify-between gap-5 pl-4 pr-4 pb-4 fixed top-32 left-0 right-0 bottom-0">
-			<div className="max-w-3xl mx-auto pt-8 pl-8 pr-8 pb-20 bg-darkGray shadow-lg rounded-lg  border border-[#334155]  flex flex-1 flex-col overflow-y-scroll">
-				<h2 className="text-2xl font-bold mb-4 text-white">
-					Add New Problem
-				</h2>
-				<div>
+		<div className="flex flex-row justify-between gap-4  ">
+			<div className="max-w-3xl  shadow-lg rounded-2xl  border-[1.5px] border-slate-800   flex flex-1 flex-col">
+				<div className="bg-slate-800 flex items-center px-5 py-4 rounded-tl-2xl rounded-tr-2xl">
+					<h2 className="text-2xl font-bold text-slate-200">
+						Add New Problem
+					</h2>
+				</div>
+				<div className=" pt-4 px-4 pb-20">
 					<div className="mb-4">
 						<label
 							className="block text-gray-200 text-sm font-bold mb-2"
@@ -88,37 +73,15 @@ export const ProblemForm = () => {
 						>
 							Problem Title
 						</label>
-						<input
+						<Input
 							type="text"
-							id="title"
-							className="w-full px-3 py-2 border rounded-md focus:outline-none  focus:ring focus:ring-offset-[#81E291] bg-transparent text-white"
+							placeholder="problem title"
+							className="focus:outline-none hover:outline-none  placeholder:text-gray-300  border-[1.5px] border-slate-800 rounded-lg"
 							value={title}
 							onChange={(e) => dispatch(setTitle(e.target.value))}
-							placeholder="Enter the problem title"
 							required
 						/>
 					</div>
-
-					<div className="mb-3">
-						<label
-							className="block text-gray-200 text-sm font-bold mb-2"
-							htmlFor="description"
-						>
-							Description
-						</label>
-						<textarea
-							id="description"
-							className=" text-white w-full px-3 py-2 border rounded-md focus:outline-none  focus:ring focus:ring-offset-[#81E291] bg-transparent"
-							value={description}
-							onChange={(e) =>
-								dispatch(setDescription(e.target.value))
-							}
-							placeholder="Describe the problem in detail"
-							rows={5}
-							required
-						></textarea>
-					</div>
-
 					<div className="mb-4">
 						<label
 							htmlFor="Difficulty"
@@ -131,13 +94,13 @@ export const ProblemForm = () => {
 								dispatch(setDifficulty(value))
 							}
 						>
-							<SelectTrigger className="w-full text-[#9ca3af]">
+							<SelectTrigger className="w-full text-[#9ca3af] border-[1.5px] border-slate-800 rounded-lg  placeholder:text-gray-300  ">
 								<SelectValue
-									placeholder="Difficulty level"
-									className="text-white"
+									placeholder="Difficulty"
+									className="text-white  placeholder:text-gray-300 "
 								/>
 							</SelectTrigger>
-							<SelectContent className="bg-darkGray text-white">
+							<SelectContent className="bg-slate-900 text-white  border-[1.5px] border-slate-700 ">
 								<SelectGroup className="">
 									<SelectItem
 										value={"Easy"}
@@ -161,7 +124,94 @@ export const ProblemForm = () => {
 							</SelectContent>
 						</Select>
 					</div>
-					<div className="mb-4">
+					<div className="mb-3">
+						<label
+							className="block text-gray-200 text-sm font-bold mb-2"
+							htmlFor="description"
+						>
+							Description
+						</label>
+						<textarea
+							id="description"
+							className=" text-white w-full px-3 py-2  border-[1.5px] hover:outline-none focus:outline-none border-slate-800  rounded-lg bg-transparent"
+							value={description}
+							onChange={(e) =>
+								dispatch(setDescription(e.target.value))
+							}
+							placeholder="Describe the problem in detail"
+							rows={5}
+							required
+						></textarea>
+					</div>
+					<div className="flex justify-end">
+						<TooltipProvider>
+							<Tooltip>
+								<TooltipTrigger asChild>
+									<button
+										disabled={isLogin ? false : true}
+										type="submit"
+										className="bg-cyan text-black font-medium px-4 py-2 rounded-md hover:bg-[#a5f3fc] focus:outline-none "
+										onClick={() =>
+											dispatch(createProblem())
+										}
+									>
+										Submit Problem
+									</button>
+								</TooltipTrigger>
+								<TooltipContent>
+									<p className="text-white">
+										{isLogin
+											? ""
+											: "You are not logged in, please login "}
+									</p>
+								</TooltipContent>
+							</Tooltip>
+						</TooltipProvider>
+					</div>
+				</div>
+			</div>
+			<TestCaseForm />
+		</div>
+	);
+};
+
+
+
+export function CustomSelectTypes({ id }: { id: string }) {
+	const dispatch = useAppDispatch();
+
+	return (
+		<Select
+			onValueChange={(value) =>
+				dispatch(setParameterType({ id, type: value }))
+			}
+		>
+			<SelectTrigger className="w-full text-[#9ca3af] bg-darkGray">
+				<SelectValue placeholder="Type" className="text-white" />
+			</SelectTrigger>
+			<SelectContent className="bg-darkGray text-white">
+				<SelectGroup className="">
+					{typeOptions.map((type, index) => {
+						return (
+							<SelectItem
+								key={index}
+								value={type}
+								className="cursor-pointer"
+							>
+								{type}
+							</SelectItem>
+						);
+					})}
+				</SelectGroup>
+			</SelectContent>
+		</Select>
+	);
+}
+
+export default ProblemForm;
+
+{
+	/* <div className="mb-4">
 						<label
 							className="block text-gray-200 text-sm font-bold mb-2"
 							htmlFor="returnType"
@@ -195,31 +245,11 @@ export const ProblemForm = () => {
 								</SelectGroup>
 							</SelectContent>
 						</Select>
-					</div>
-					<div className="mb-4">
-						<label className="block text-gray-200 text-sm font-bold mb-2">
-							Add Parameters name and return type
-						</label>
-						<Button
-							classname="flex items-center justify-between bg-transparent border rounded-md focus:outline-none  focus:ring focus:ring-offset-[#81E291] w-full hover:bg-[#ecfeff] text-[#9ca3af] hover:text-black"
-							onClick={() => handleParameters()}
-						>
-							<span className="text-md ">Add new Parameter</span>
-							<IoAdd className="font-medium text-xl" />
-						</Button>
-					</div>
-					<div className="mb-4 flex flex-col gap-2">
-						{parameters.map((parameter, index) => {
-							return (
-								<ParameterCotnainer
-									key={index}
-									id={parameter.parameterId}
-									name={parameter.name}
-								/>
-							);
-						})}
-					</div>
-					<div className="mb-4">
+					</div> */
+}
+
+{
+	/* <div className="mb-4">
 						<label
 							className="block text-gray-200 text-sm font-bold mb-2"
 							htmlFor="testcase"
@@ -233,115 +263,8 @@ export const ProblemForm = () => {
 							<span className="text-md ">Add new TestCase</span>
 							<IoAdd className="font-medium text-xl" />
 						</Button>
-					</div>
-					<div className="mb-4 flex flex-col gap-2">
-						{testcases.map((testcase, index) => {
-							return (
-								<TestCaseContainer
-									key={index}
-									id={testcase.testcaseId}
-									testcaseNo={index + 1}
-									inputs={testcase.inputs}
-									output={testcase.output}
-								/>
-							);
-						})}
-					</div>
-
-					<div className="flex justify-end">
-						<TooltipProvider>
-							<Tooltip>
-								<TooltipTrigger asChild>
-									<button
-										disabled={isLogin ? false : true}
-										type="submit"
-										className="bg-cyan text-black font-medium px-4 py-2 rounded-md hover:bg-[#a5f3fc] focus:outline-none "
-										onClick={() =>
-											dispatch(createProblem())
-										}
-									>
-										Submit Problem
-									</button>
-								</TooltipTrigger>
-								<TooltipContent>
-									<p className="text-white">
-										{isLogin
-											? ""
-											: "You are not logged in, please login "}
-									</p>
-								</TooltipContent>
-							</Tooltip>
-						</TooltipProvider>
-					</div>
-				</div>
-			</div>
-
-			<div className="text-white flex flex-1 bg-darkGray shadow-lg rounded-lg  border border-[#334155] ">
-				<ContributionExample />
-			</div>
-		</div>
-	);
-};
-
-function ParameterCotnainer({ id, name }: { id: string; name: string }) {
-	const dispatch = useAppDispatch();
-
-	return (
-		<div className="flex flex-row gap-2 bg-[#1f2937] px-3 py-3 rounded-md ">
-			<div className="flex flex-1">
-				<CustomSelectTypes id={id} />
-			</div>
-
-			<div className="flex flex-1">
-				<input
-					type="text"
-					id="testcase"
-					className="text-white w-full px-3  border rounded-md focus:outline-none  focus:ring focus:ring-offset-[#81E291] bg-darkGray"
-					value={name}
-					onChange={(e) =>
-						dispatch(setparameterName({ id, name: e.target.value }))
-					}
-					placeholder="Name"
-					required
-				/>
-			</div>
-			<div
-				className="flex items-center cursor-pointer bg-darkGray px-1.5 rounded-lg"
-				onClick={() => dispatch(deleteParameter({ id }))}
-			>
-				<MdDelete className=" text-white w-5 h-5 " />
-			</div>
-		</div>
-	);
+					</div> */
 }
-
-function CustomSelectTypes({ id }: { id: string }) {
-	const dispatch = useAppDispatch();
-
-	return (
-		<Select
-			onValueChange={(value) =>
-				dispatch(setParameterType({ id, type: value }))
-			}
-		>
-			<SelectTrigger className="w-full text-[#9ca3af] bg-darkGray">
-				<SelectValue placeholder="Type" className="text-white" />
-			</SelectTrigger>
-			<SelectContent className="bg-darkGray text-white">
-				<SelectGroup className="">
-					{typeOptions.map((type, index) => {
-						return (
-							<SelectItem
-								key={index}
-								value={type}
-								className="cursor-pointer"
-							>
-								{type}
-							</SelectItem>
-						);
-					})}
-				</SelectGroup>
-			</SelectContent>
-		</Select>
-	);
+{
+	
 }
