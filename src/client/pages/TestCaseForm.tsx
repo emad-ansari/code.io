437f33956643e9a37ae262d7db76528c39677780
92Mohammad <m.e89571828@gmail.com>
2024-11-17 10:39:36 +0530

    new design of testcase form

--- src/client/pages/TestCaseForm.tsx
+++ src/client/pages/TestCaseForm.tsx
@@ -1,102 +1,49 @@
 import { useSelector } from "react-redux";
-import { v4 as uuidv4 } from 'uuid';
-import { IoAdd } from "react-icons/io5";
+import { Plus } from "lucide-react";
 
 import { useAppDispatch, RootState } from "@/client/app/store";
+import { addNewTestcase } from "@/client/features/problemFormSlice";
 import { TestCaseContainer } from "@/client/components/common/TestCaseContainer";
-import { Button } from "@/client/components/common/Button";
-import { addNewTestCase, setProblemTitle } from "@/client/features/TestcaseSlice";
+import { Button } from "../components/ui/button";
 
 export const TestCaseForm = () => {
 	const dispatch = useAppDispatch();
-	const { problemTitle, testcases} = useSelector((state: RootState) => state.TestCaseForm);
+
+	const { testcases } = useSelector((state: RootState) => state.problemform);
 	
-
-  const handleNewTestCase = () => {
-		const newTestCase = {
-			testcaseId: uuidv4(),
-			inputs: [
-				{
-					inputId: uuidv4(),
-					name: "",
-					type: "",
-					value: "",
-				},
-			],
-			output: {
-				type: "",
-				value: "",
-			},
-		};
-		dispatch(addNewTestCase(newTestCase));
-	};
-  
 	return (
-		<div className="bg-PRIMARY fixed top-32 bottom-0 left-0 right-0 flex flex-row justify-between gap-5 pl-4 pr-4 pb-4">
-			<div className="max-w-3xl mx-auto pt-8 pl-8 pr-8 pb-20 bg-darkGray shadow-lg rounded-lg  border border-[#334155]  flex flex-1 flex-col overflow-y-scroll">
-				<h2 className="text-2xl font-bold mb-4 text-white">
-					Add New Test Case Here
+		<div className="max-w-3xl  shadow-lg rounded-2xl  border-[1.5px] border-slate-800   flex flex-1 flex-col">
+			<div className="bg-slate-800 flex items-center px-5 py-4 rounded-tl-2xl rounded-tr-2xl">
+				<h2 className="text-2xl font-bold text-slate-200">
+					Testcase Examples Details
 				</h2>
-				<div>
-					<div className="mb-4">
-						<label
-							className="block text-gray-200 text-sm font-bold mb-2"
-							htmlFor="title"
-						>
-							Problem Title
-						</label>
-						<input
-							type="text"
-							id="title"
-							className="w-full px-3 py-2 border rounded-md focus:outline-none  focus:ring focus:ring-offset-[#81E291] bg-transparent text-white"
-							value={problemTitle}
-							onChange={(e) => dispatch(setProblemTitle(e.target.value))}
-							placeholder="Enter the problem title"
-							required
-						/>
-					</div>
-
-					<div className="mb-4">
-						<label
-							className="block text-gray-200 text-sm font-bold mb-2"
-							htmlFor="testcase"
-						>
-							Add Test Case Exmaples
-						</label>
-						<Button
-							classname="flex items-center justify-between bg-transparent border rounded-md focus:outline-none  focus:ring focus:ring-offset-[#81E291] w-full hover:bg-[#ecfeff] text-[#9ca3af] hover:text-black"
-							onClick={() => handleNewTestCase()}
-						>
-							<span className="text-md ">Add new TestCase</span>
-							<IoAdd className="font-medium text-xl" />
-						</Button>
-					</div>
-					<div className="mb-4 flex flex-col gap-2">
-						{testcases.map((testcase, index) => {
-							return (
-								<TestCaseContainer
-									key={index}
-									id={testcase.testcaseId}
-									testcaseNo={index + 1}
-									inputs={testcase.inputs}
-									output={testcase.output}
-								/>
-							);
-						})}
-					</div>
-
-					<div className="flex justify-end">
-						<button
-							type="submit"
-							className="bg-cyan text-black font-medium px-4 py-2 rounded-md hover:bg-[#a5f3fc] focus:outline-none  focus:ring focus:ring-offset-[#81E291]"
-						>
-							Submit Problem
-						</button>
-					</div>
-				</div>
 			</div>
-			<div className="text-white flex flex-1 bg-darkGray shadow-lg rounded-lg  border border-[#334155] ">
-				<h1>Example</h1>
+			<div className={`flex flex-col justify-start ${testcases.length === 0 && " items-center pt-10"}`}>
+				<div className=" px-4 py-4 flex flex-col gap-4 text-lg">
+					{testcases.length === 0 ? (
+						<div>No testcase added yet</div>
+					) : (
+						testcases.map((testcase, index) => (
+							<TestCaseContainer
+								key={testcase.id}
+								id={testcase.id}
+								testcaseNo={index + 1}
+								input={testcase.input}
+								output={testcase.output}
+							/>
+						))
+					)}
+				</div>
+
+				<div className="px-4 flex justify-center">
+					<Button
+						className="flex flex-row gap-3 bg-slate-800 rounded-xl "
+						onClick={() => dispatch(addNewTestcase())}
+					>
+						<Plus className="w-4 h-4" />
+						Add More Testcase
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
