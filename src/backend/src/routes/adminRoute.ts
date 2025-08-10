e864ecf9baef41fed725fbc4ed78afba94939366
92Mohammad <m.e89571828@gmail.com>
2025-08-10 14:13:56 +0530

    updated route handle to clean name

--- src/backend/src/routes/adminRoute.ts
+++ src/backend/src/routes/adminRoute.ts
@@ -8,15 +8,13 @@ import {
 } from "../@utils/types";
 import {
 	createNewProblemCategory,
-	getAllCategory,
+	getAllCategoryOnAdminPage,
 } from "../db/problem-category";
-import {
-	createProblem,
-	getProblemsOnAdminPage,
-} from "../db/problem";
+import { createProblem, getProblemsOnAdminPage } from "../db/problem";
+import { getAllUsers } from "../db/user";
 
 // CREATE: new problem category
-router.post("/create-problem-category", async (req: Request, res: Response) => {
+router.post("/create-category", async (req: Request, res: Response) => {
 	const { userAuthorized, role } = req as CustomRequestObject;
 
 	if (!userAuthorized || role != "ADMIN") {
@@ -26,22 +24,25 @@ router.post("/create-problem-category", async (req: Request, res: Response) => {
 		});
 	}
 	try {
-		const parsedInput = ProblemCategorySchema.safeParse(req.body);
+		const parsedInput = ProblemCategorySchema.safeParse(req.body.data);
 		if (!parsedInput.success) {
 			return res.json({
 				success: false,
 				msg: parsedInput.error,
 			});
 		}
-
+		console.log('parsed data: ', parsedInput.data)
 		const result = await createNewProblemCategory(parsedInput.data);
-		return res.json({ success: result.success, msg: result.msg });
+		return res.json({
+			success: result.success,
+			msg: result.msg,
+		});
 	} catch (error: any) {
 		console.log("CREATE_CATEGORY_ERROR", error);
 	}
 });
 
-// GET: All Category
+// GET: All Category ON ADMIN PAGE
 router.post("/get-all-category", async (req: Request, res: Response) => {
 	const { userAuthorized, role } = req as CustomRequestObject;
 
@@ -52,7 +53,7 @@ router.post("/get-all-category", async (req: Request, res: Response) => {
 		});
 	}
 	try {
-		const result = await getAllCategory();
+		const result = await getAllCategoryOnAdminPage();
 		return res.json({
 			success: true,
 			msg: "All Categories",
@@ -109,16 +110,34 @@ router.get("get-all-problems", async (req: Request, res: Response) => {
 	}
 	try {
 		const problems = getProblemsOnAdminPage(page);
-		return res
-			.status(200)
-			.json({
-				success: true,
-				msg: "Successfully fetched all  Problems",
-				data: problems,
-			});
+		return res.status(200).json({
+			success: true,
+			msg: "Successfully fetched all  Problems",
+			data: problems,
+		});
 	} catch (error: any) {
 		console.log("GET-ALL_PROBLMES_ERROR", error);
 	}
+});
+
+// GET ALL USERS route
+router.get("/get-all-users", async (req: Request, res: Response) => {
+	const { userAuthorized, role } = req as CustomRequestObject;
+	const { page } = req.body;
+
+	if (!userAuthorized || role != "ADMIN") {
+		return res.status(404).json({
+			success: false,
+			msg: "UnAuthorized Access!!",
+		});
+	}
+
+	try {
+		const users = getAllUsers(Number(page));
+		return res
+			.status(200)
+			.json({ success: true, msg: "All users", data: users });
+	} catch (error: any) {}
 });
 
 export default router;
