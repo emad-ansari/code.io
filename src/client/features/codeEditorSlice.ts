8b94163da63b55147562cd0c43a88f42fac2b0ea
92Mohammad <m.e89571828@gmail.com>
2025-08-16 16:13:09 +0530

    added fetch default code action

--- src/client/features/codeEditorSlice.ts
+++ src/client/features/codeEditorSlice.ts
@@ -2,42 +2,35 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { api } from "@/client/api/client";
 import {
 	EditorState,
-	CodeExecutionResponse,
 	APIResponse,
 	DefaultCodeProps
 } from "@/client/lib/types";
 
 export const codeEditorState: EditorState = {
+	loading: false,
+	error: null,
 	isFullScreen: false,
 	language: localStorage.getItem("selectedLanguage") || "java",
 	code: "",
-	execution_result: {
-		overallStatus: "",
-		passed_testcases: -1,
-		submissions: [],
-	},
-	error: null,
-	loading: false,
 };
 
 
 export const fetchDefaultCode = createAsyncThunk<
-	APIResponse<{ defaultCode: string }>,
+	APIResponse<{boiler_code: string}>,
 	DefaultCodeProps
 >(
 	"/editor/fetchDefaultCode",
-	async ({ problemTitle, languageId }: DefaultCodeProps, thunkAPI) => {
+	async ({ problemId, language }: DefaultCodeProps, thunkAPI) => {
 		try {
-			const res = await api.get<APIResponse<{ defaultCode: string }>>(
-				"/problem/default-code",
-				{
-					params: {
-						problemTitle,
-						languageId,
-					},
+			const res = await api.get('/problem/default-code', {
+				params: {
+					language,
+					problemId
 				}
-			);
+			})
+			console.log('default code: ', res.data)
 			return res.data;
+
 		} catch (error: any) {
 			return thunkAPI.rejectWithValue(
 				error.message || "Error occurred while fetching default code."
@@ -46,49 +39,49 @@ export const fetchDefaultCode = createAsyncThunk<
 	}
 );
 
-export const runCode = createAsyncThunk(
-	"/editor/runCode",
-	async ({ problemTitle, languageId, code }: DefaultCodeProps, thunkAPI) => {
-		try {
-			const res = await api.post<CodeExecutionResponse>(
-				"/submission/run-code",
-				{
-					data: {
-						problemTitle,
-						languageId,
-						code: code,
-					},
-				}
-			);
-			return res.data;
-		} catch (error: any) {
-			console.log(error.message);
-			return thunkAPI.rejectWithValue(error.message || "Error while  running code.");
-		}
-	}
-);
+// export const runCode = createAsyncThunk(
+// 	"/editor/runCode",
+// 	async ({ problemTitle, languageId, code }: DefaultCodeProps, thunkAPI) => {
+// 		try {
+// 			const res = await api.post<CodeExecutionResponse>(
+// 				"/submission/run-code",
+// 				{
+// 					data: {
+// 						problemTitle,
+// 						languageId,
+// 						code: code,
+// 					},
+// 				}
+// 			);
+// 			return res.data;
+// 		} catch (error: any) {
+// 			console.log(error.message);
+// 			return thunkAPI.rejectWithValue(error.message || "Error while  running code.");
+// 		}
+// 	}
+// );
 
-export const submitCode = createAsyncThunk(
-	"/editor/submitCode",
-	async ({ problemTitle, languageId, code }: DefaultCodeProps, thunkAPI) => {
-		try {
-			const res = await api.post<CodeExecutionResponse>(
-				"/submission/submit-code",
-				{
-					data: {
-						problemTitle,
-						languageId,
-						code: code,
-					},
-				}
-			);
-			return res.data;
-		} catch (error: any) {
-			console.log(error.message);
-			return thunkAPI.rejectWithValue(error.message || "Error while  running code.");
-		}
-	}
-);
+// export const submitCode = createAsyncThunk(
+// 	"/editor/submitCode",
+// 	async ({ problemTitle, languageId, code }: DefaultCodeProps, thunkAPI) => {
+// 		try {
+// 			const res = await api.post<CodeExecutionResponse>(
+// 				"/submission/submit-code",
+// 				{
+// 					data: {
+// 						problemTitle,
+// 						languageId,
+// 						code: code,
+// 					},
+// 				}
+// 			);
+// 			return res.data;
+// 		} catch (error: any) {
+// 			console.log(error.message);
+// 			return thunkAPI.rejectWithValue(error.message || "Error while  running code.");
+// 		}
+// 	}
+// );
 
 
 
@@ -111,45 +104,44 @@ export const codeEditorSlice = createSlice({
 			console.log(action.payload);
 		});
 		builder.addCase(fetchDefaultCode.fulfilled, (state, action) => {
-			const { success, message, data } = action.payload;
-			console.log("message of deffault code: ", message);
+			const { success, msg, data } = action.payload;
+			console.log("message of deffault code: ", msg);
 			if (success && data) {
-				const { defaultCode } = data;
-				state.code = defaultCode;
+				state.code = data.boiler_code
 			}
 		});
 		builder.addCase(fetchDefaultCode.rejected, (_, action) => {
 			console.log(action.payload);
 		});
-		builder.addCase(runCode.pending, (state) => {
-			state.loading = true;
-		});
-		builder.addCase(runCode.fulfilled, (state, action) => {
-			const { success, data } = action.payload;
-			if (success) {
-				state.execution_result = data;
-				state.loading = false;
-			}
-		});
-		builder.addCase(runCode.rejected, (state, action) => {
-			state.loading = false;
-			state.error = action.error.message;
-		});
-		builder.addCase(submitCode.pending, (state) => {
-			state.loading = true;
-		});
-		builder.addCase(submitCode.fulfilled, (state, action) => {
-			const { success, data } = action.payload;
-			if (success) {
-				state.execution_result = data;
-				state.loading = false;
-				console.log("submit code api response: ", data );
-			}
-		});
-		builder.addCase(submitCode.rejected, (state, action) => {
-			state.loading = false;
-			state.error = action.error.message;
-		});
+		// builder.addCase(runCode.pending, (state) => {
+		// 	state.loading = true;
+		// });
+		// builder.addCase(runCode.fulfilled, (state, action) => {
+		// 	const { success } = action.payload;
+		// 	if (success) {
+		// 		// state.execution_result = data;
+		// 		state.loading = false;
+		// 	}
+		// });
+		// builder.addCase(runCode.rejected, (state, action) => {
+		// 	state.loading = false;
+		// 	state.error = action.error.message;
+		// });
+		// builder.addCase(submitCode.pending, (state) => {
+		// 	state.loading = true;
+		// });
+		// builder.addCase(submitCode.fulfilled, (state, action) => {
+		// 	const { success, data } = action.payload;
+		// 	if (success) {
+		// 		// state.execution_result = data;
+		// 		state.loading = false;
+		// 		console.log("submit code api response: ", data );
+		// 	}
+		// });
+		// builder.addCase(submitCode.rejected, (state, action) => {
+		// 	state.loading = false;
+		// 	state.error = action.error.message;
+		// });
 	},
 });
 
