da1898ba964077701039900dee25b0c0a4cdd614
92Mohammad <m.e89571828@gmail.com>
2025-08-17 22:38:20 +0530

    added SubmissionStatus type

--- src/client/lib/types.ts
+++ src/client/lib/types.ts
@@ -1,3 +1,13 @@
+export interface ProblemCategory {
+	id: string;
+	name: string;
+	title: string;
+	tags: string[];
+	imgUrl: string;
+	totalProblems: number;
+	solvedProblems: number;
+}
+
 export interface Testcase {
 	id: string;
 	input: string;
@@ -37,10 +47,9 @@ export interface ProblemState {
 	problems: Problem[];
 	problemDetail: ProblemDetail | null;
 	likes: number;
-	dislikes: number
-	userSubmissions: UserSubmission[]
+	dislikes: number;
+	userSubmissions: UserSubmission[];
 }
-
 
 export interface ProblemDetail {
 	id: string | undefined;
@@ -50,7 +59,7 @@ export interface ProblemDetail {
 	difficulty: string | undefined;
 	likes: number | undefined;
 	tags: string[] | undefined;
-	status: string | null
+	status: string | null;
 }
 
 export interface UserSubmission {
@@ -60,7 +69,7 @@ export interface UserSubmission {
 	time: string;
 	memory: string;
 	status: string;
-	createdAt: Date
+	createdAt: Date;
 }
 
 export interface SettingState {
@@ -72,20 +81,11 @@ export interface DropDownType {
 	isDifficultyMenuOpen: boolean;
 }
 
-// export interface EditorState {
-// 	language: string;
-// 	isFullScreen: boolean;
-// 	code: string;
-// 	execution_result: ExecutionResult;
-// 	loading: boolean;
-// 	error: string | null | undefined;
-// }
 export interface DefaultCodeProps {
-	problemTitle: string;
-	languageId: number;
+	problemId: string;
+	language: string;
 	code?: string;
 }
-
 export interface FilterState {
 	easy: boolean;
 	medium: boolean;
@@ -93,67 +93,53 @@ export interface FilterState {
 }
 
 export interface FetchProblemProps {
-	categoryName: string,
+	categoryName: string;
 	pageNumber: number;
 	difficulty: string;
 	status: string;
 	searchKeywords: string;
 }
 
-// export interface CodeExecutionResponse {
-// 	success: boolean;
-// 	data: ExecutionResult;
-// 	message?: string;
-// }
+export interface EditorState {
+	loading: boolean;
+	error: string | null | undefined;
+	isFullScreen: boolean;
+	language: string;
+	code: string;
+	submission_result: CodeExecutionResult;
+}
 
-// export interface ExecutionResult {
-// 	overallStatus: string; // Wrong Answer || Compilation Error || Accepted || Time Limit Exceed
-// 	passed_testcases: number;
-// 	submissions: SubmissionDetails[];
-// }
+export interface CodeExecutionResult {
+	passed_testcases: number;
+	submissions: SubmissionDetails[];
+}
 
-// export interface SubmissionDetails {
-// 	languageId: number;
-// 	stdin: string;
-// 	stdout: string;
-// 	expected_output: string;
-// 	status: {
-// 		id: number;
-// 		description: string;
-// 	};
-// 	compile_output?: string | null;
-// 	testcaseExamples?: TestCaseExample[];
-// 	inputs: TestCaseInput[];
-// }
+export interface SubmissionDetails {
+	input: string; // stdin
+	user_output: string; // stdout
+	expected_output: string; // expected output
+	status: SubmissionStatus;
+	compile_output?: string | null;
+}
+
+export interface CodeExecutionProps {
+	language: string;
+	problemId: string;
+	code: string;
+}
 
 export interface APIResponse<T> {
 	success: boolean;
-	data?: T;	
-	msg: string | null
+	data?: T;
+	msg: string | null;
 }
 
-export interface ProblemCategory {
-	id: string;
-	name: string;
-	title: string;
-	tags: string[];
-	imgUrl: string;
-	totalProblems: number;
-	solvedProblems: number
-}
-
-
-export const LNAGUAGE_MAPPING: {
-	[key: string]: { name: string; languageId: number };
-} = {
-	javascript: { name: "javascript", languageId: 63 },
-	cpp: { name: "cpp", languageId: 54 },
-	typescript: { name: "typescript", languageId: 74 },
-	java: { name: "java", languageId: 62 },
-	python: { name: "python", languageId: 71 },
-};
-
-export const EDITOR_THEMES: string[] = ["vs-dark", "vs", "hc-light", "OneDarkPro"];
+export const EDITOR_THEMES: string[] = [
+	"vs-dark",
+	"vs",
+	"hc-light",
+	"OneDarkPro",
+];
 
 export const FONT_SIZES: string[] = ["14px", "16px", "18px", "20px", "22px"];
 
@@ -161,11 +147,19 @@ export const LANGUAGES = ["java", "cpp", "python"];
 
 export const problems_per_page = ["10 / page", "20 / page", "50 / page"];
 
-export const DIFFICULTY = ['Easy', 'Medium', 'Hard'];
+export const DIFFICULTY = ["Easy", "Medium", "Hard"];
 
-export const STATUS =  ["Todo", "Solved", "Attempted"]
+export const STATUS = ["Todo", "Solved", "Attempted"];
 
+export type SubmissionStatus =
+	| "Accepted"
+	| "Wrong Answer"
+	| "Compilation Error"
+	| "Time Limit Exceeded"
+	| "Memory Limit Exceeded"
+	| "Run Time Error";
 
-// export const formatNumbers = (value: number | undefined) =>  {
-// 	return formatSiUnit(value);
-// }
+export const successClassname =
+	"max-w-full  flex gap-3 px-2 py-1 items-center bg-green-800/10 text-green-500 rounded-lg border border-green-500/10 cursor-pointer";
+
+export const errorClassname = "max-w-full flex gap-3 px-2 py-1 items-center bg-red-800/10 text-red-500 rounded-lg border border-red-500/10 cursor-pointer"
