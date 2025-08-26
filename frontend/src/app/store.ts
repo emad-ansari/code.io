2855b1d406ed90aa003d65bcb1966f78a775cea5
92Mohammad <m.e89571828@gmail.com>
2025-08-26 12:08:27 +0530

    removed unused reducer

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,24 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
-
 import problemReducer from "@/features/problemSlice";
-import dropDownReducer from "@/features/dropDownSlice";
-import filterReducer from "@/features/filterSlice";
 import settingReducer from "@/features/editorSettingSlice";
 import editorReducer from "@/features/codeEditorSlice";
 import userSlice from "@/features/authSlice";
-import ProblemCategorySlice from "@/features/problemCategorySlice"
-
+import ProblemCategorySlice from "@/features/problemCategorySlice";
 
 export const store = configureStore({
 	reducer: {
 		problem: problemReducer,
-		dropdown: dropDownReducer,
-		filter: filterReducer,
 		setting: settingReducer,
 		editor: editorReducer,
 		user: userSlice,
-		problem_category :ProblemCategorySlice
+		problem_category: ProblemCategorySlice,
 	},
 });
 
