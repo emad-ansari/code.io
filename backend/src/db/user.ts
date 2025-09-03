c845f8d46e01484bb3d820c541905d8c275e717e
92Mohammad <m.e89571828@gmail.com>
2025-09-03 16:06:45 +0530

    added getUserProfile route

--- backend/src/db/user.ts
+++ backend/src/db/user.ts
@@ -1,3 +1,4 @@
+import { escape } from "querystring";
 import prisma from ".";
 import bcrypt from "bcryptjs";
 
@@ -101,5 +102,78 @@ export async function getAllUsers(page: number) {
 	}
 	catch(error: any) {
 		console.log("GET_ALL_USERS_DB_ERROR", error);
+	}
+}
+
+export async function getUserProfile (userId: string) {
+	try {
+
+		// get total easy, medium and hard problem, and total problem also
+		const totalEasy  = await prisma.problem.count({
+			where: {
+				difficulty: "Easy"
+			}
+		});
+		const totalMedium = await prisma.problem.count({
+			where: {
+				difficulty: "Medium"
+			}
+		})
+		const totalHard = await prisma.problem.count({
+			where: {
+				difficulty: "Hard"
+			}
+		})
+
+		const totalProblems = await prisma.problem.count();
+
+		const solvedProblems = await prisma.user.findFirst({
+			where: {
+				id: userId,
+			},
+			select: {
+				username: true,
+				progress: {
+					select: {
+						easySolved: true,
+						mediumSolved: true,
+						hardSolved: true,
+						totalSolved: true
+					}
+				},
+				streak: {
+					select: {
+						current: true,
+						longest: true
+					}
+				}
+			}
+		})
+
+		return {
+			username: solvedProblems?.username,
+			easyProgress:{
+				solved: solvedProblems?.progress?.easySolved,
+				total: totalEasy
+			},
+			mediumProgress: {
+				solved: solvedProblems?.progress?.mediumSolved,
+				total: totalMedium
+			},
+			hardProgress: {
+				solved: solvedProblems?.progress?.hardSolved,
+				total: totalHard
+			},
+			overAllProgress: {
+				solved: solvedProblems?.progress?.totalSolved,
+				total: totalProblems
+			},
+			currentStreak: solvedProblems?.streak?.current,
+			longestStreak: solvedProblems?.streak?.longest
+		}
+	}
+	catch(err: any) {
+		console.log("GET_USER_PROIFLE_DB_ERROR: ", err);
+		throw new Error(err);
 	}
 }
--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import prisma from "../db";
 import { CustomRequestObject } from "../middleware/auth";
+import { getUserProfile } from "../db/user";
 
 const router = express.Router();
 
@@ -31,5 +32,30 @@ router.get("/get-user-details", async (req: Request, res: Response) => {
 			.json({ success: false, msg: "Internal server error" });
 	}
 });
+
+router.get('/profile', async(req: Request, res: Response) => {
+	const {userAuthorized, userId} = req as CustomRequestObject;
+	if (!userAuthorized) {
+		return res.status(401).json({
+			success: false,
+			msg: "Unauthorized Access!"
+		})
+	}
+	try {
+		const userProfile = await getUserProfile(userId);
+		return res.status(200).json({
+			success: true,
+			msg: "Profile Fetched Successfully",
+			data: userProfile
+		})
+	}
+	catch(error: any) {
+		console.log("GET_USER_PROFILE_ROUTE_ERROR: ", error);
+		return res.status(500).json({
+			success: false,
+			msg: error
+		})
+	}
+})
 
 export default router;
