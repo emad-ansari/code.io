791117791942016a82d829929a49370bb9d8f8c0
92Mohammad <m.e89571828@gmail.com>
2026-04-03 00:45:18 +0530

    added email validation method

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const validateEmail = (value: string) => {
+		const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		return regex.test(value);
+	};
